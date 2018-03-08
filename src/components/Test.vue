d5207ddfe527e37bdcb9a37e2d2582b69eec5a86
songxiaoxu <913581148@qq.com>
2018-03-08 15:15:11 +0800

    菜品后台，VUE项目demo

---
+++ src/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <p>{{ data[0] }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to your frist test',
+      data: ['songxiaoxu', 23],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background: blue;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
